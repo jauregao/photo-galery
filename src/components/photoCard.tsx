772cf76c638fcf75a69e06ca755086fa824a4e59
Amanda Jauregui <amandajauregui.dev@gmail.com>
2025-01-06 15:26:37 -0300

    css ajustments

--- src/components/photoCard.tsx
+++ src/components/photoCard.tsx
@@ -5,30 +5,35 @@ import Image from "next/image";
 export function PhotoCard({ pokemon }: { pokemon: TPokemon }) {
   return (
     <motion.div
-      className="opacity-80 w-40 mx-auto flex flex-col"
+      className="opacity-90 w-full mx-auto flex flex-col"
       whileHover={{
         scale: 1.2,
         opacity: 1,
         position: "relative",
-        zIndex: 99
+        zIndex: 99,
+        color: "#fff",
+        fontWeight: "bold",
       }}
       transition={{
-        duration: 0.4
+        duration: 0.2
       }}
+
     >
       <motion.div
-        className="w-40 mx-auto p-4 bg-[--foreground] rounded-xl"
+        className="w-full mx-auto p-4 bg-[--foreground] rounded-xl"
+        initial={{ rotate: 0 }}
         whileHover={{
-          rotate: 360,
-          boxShadow: "10px 10px 10px rgba(0, 0, 0, 0.2)",
-          background: "--secondary"
+          rotate: [0, 5, -5, 5, -5, 0],
+          boxShadow: "10px 10px 10px rgba(0, 0, 0, 0.4)",
+          border: "4px solid var(--secondary)",
         }}
         transition={{
-          duration: 0.4
+          duration: 0.2
         }}
       >
+
         <Image
-          className="w-full h-auto"
+          className="w-full h-auto opacity-100"
           alt={`${pokemon.name} photo.`}
           src={pokemon.photo}
           width={100}
