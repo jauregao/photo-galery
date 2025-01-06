821ab406e1de9aca8d910eb95e4d600ff7d2a475
Amanda Jauregui <amandajauregui.dev@gmail.com>
2025-01-06 15:27:53 -0300

    added not found text

--- src/components/photoGrid.tsx
+++ src/components/photoGrid.tsx
@@ -4,8 +4,13 @@ import { TPokemon } from "@/types/character";
 export default function PhotoGrid({ characters }: { characters: TPokemon[] }) {
   return (
     <main
-      className="grid grid-cols-3 grid-rows-3 max-w-screen-lg mx-auto gap-8 py-8 "
+      className="grid grid-cols-4 max-w-screen-lg mx-auto gap-12 py-12 w-full overflow-x-visible"
     >
+      {
+        characters.length === 0 && <h1
+          className="col-start-1 col-end-5 w-full text-center text-2xl row-start-1 row-end-11 self-center"
+        >{`Não foi encontrado nenhum pokémon com esse nome :(`}<br />tente outro!</h1>
+      }
       {
         characters?.map(character => {
           return (
